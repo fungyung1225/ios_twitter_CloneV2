8d3de79d67be6c5279d14fb981a7a51adf9da952
Fung Yung <fungyung1225@gmamil.com>
2019-03-14 01:06:50 -0400

    add new view add new view controller add Tweet btn on Home Controller add Canel and Tweet btn on Tweet controller add Text View on Tweet View

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Fung on 3/14/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancelBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tweetBtn(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
